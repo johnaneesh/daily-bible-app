e114d8291d45fc4d225a7793c6a6f100a7590a1b
johnaneesh <johnjohn2a@gmail.com>
2026-02-07 22:03:31 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'bible-v1';
+const assets = ['./index.html', './manifest.json'];
+
+// Install the service worker and cache files
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch files from cache when offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
